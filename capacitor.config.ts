fc9f7c440b5ea430afe917c796e32916a1f2219b
MARCELO COLLADO <90863579+Collado66@users.noreply.github.com>
2026-05-25 20:28:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-crud-standalone',
+  webDir: 'www'
+};
+
+export default config;
